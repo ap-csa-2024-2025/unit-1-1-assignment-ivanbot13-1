8881063f35ac873cb15fafa4a5c6184a453987c1
ivanbot13 <ikduong100@student.hbuhsd.edu>
2024-09-04 10:38:05 -0700

    prob1

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,15 @@ public class HelloWorld
 {
   public static void main(String[] args)
   {
-    System.out.print("Hello world!");
+    System.out.println("Hello world!");
+
+
+    int stock = 1;
+    double stockPrice = 99.99;
+    String buyer = "how much would you like to buy";
+    boolean hasStock = true;
+
+
+    System.out.println("Hello good sir, " + buyer);
   }
 }
